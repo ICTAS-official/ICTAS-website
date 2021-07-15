2150335e6d68c5f1283b83cdeff91edaa0b9e5fb
abishek sriram <abisheksriram2001@gmail.com>
2021-07-15 13:43:31 +0530

    change log 5 in change log.txt

--- web dev/script.js
+++ web dev/script.js
@@ -45,38 +45,38 @@ function changeMemberList(n)
     }
 }
 
-var currentTeam="filter-tech";
-function changeTeamList(n)
-{
-    if(n==1)
-    {
+// var currentTeam="filter-tech";
+// function changeTeamList(n)
+// {
+//     if(n==1)
+//     {
 
-        document.getElementById(currentTeam).className=String(document.getElementById(currentTeam).className).replace("active","");
-        currentTeam="filter-tech";
-        document.getElementById(currentTeam).className=document.getElementById(currentTeam).className+"active";
-        document.getElementById("l-tech").hidden=false;
-        document.getElementById("l-nontech").hidden=true;
-        document.getElementById("l-pr").hidden=true;
-    }
-    if(n==2)
-    {
-        document.getElementById(currentTeam).className=  String(document.getElementById(currentTeam).className).replace("active","");
-        currentTeam="filter-nontech";
-        document.getElementById(currentTeam).className=document.getElementById(currentTeam).className+" active";
-        document.getElementById("l-tech").hidden=true;
-        document.getElementById("l-nontech").hidden=false;
-        document.getElementById("l-pr").hidden=true;
-    }
-    if(n==3)
-    {
-        document.getElementById(currentTeam).className=  String(document.getElementById(currentTeam).className).replace("active","");
-        currentTeam="filter-pr";
-        document.getElementById(currentTeam).className=document.getElementById(currentTeam).className+" active";
-        document.getElementById("l-tech").hidden=true;
-        document.getElementById("l-nontech").hidden=true;
-        document.getElementById("l-pr").hidden=false;
-    }
-}
+//         document.getElementById(currentTeam).className=String(document.getElementById(currentTeam).className).replace("active","");
+//         currentTeam="filter-tech";
+//         document.getElementById(currentTeam).className=document.getElementById(currentTeam).className+"active";
+//         document.getElementById("l-tech").hidden=false;
+//         document.getElementById("l-nontech").hidden=true;
+//         document.getElementById("l-pr").hidden=true;
+//     }
+//     if(n==2)
+//     {
+//         document.getElementById(currentTeam).className=  String(document.getElementById(currentTeam).className).replace("active","");
+//         currentTeam="filter-nontech";
+//         document.getElementById(currentTeam).className=document.getElementById(currentTeam).className+" active";
+//         document.getElementById("l-tech").hidden=true;
+//         document.getElementById("l-nontech").hidden=false;
+//         document.getElementById("l-pr").hidden=true;
+//     }
+//     if(n==3)
+//     {
+//         document.getElementById(currentTeam).className=  String(document.getElementById(currentTeam).className).replace("active","");
+//         currentTeam="filter-pr";
+//         document.getElementById(currentTeam).className=document.getElementById(currentTeam).className+" active";
+//         document.getElementById("l-tech").hidden=true;
+//         document.getElementById("l-nontech").hidden=true;
+//         document.getElementById("l-pr").hidden=false;
+//     }
+// }
 //Get the button
 
 // When the user scrolls down 20px from the top of the document, show the button

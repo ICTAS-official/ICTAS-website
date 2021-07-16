3fe41eeb978f85e6197b6bc707a5654daf321eb3
abishek sriram <abisheksriram2001@gmail.com>
2021-07-16 16:58:26 +0530

    temporary commit changed navbar with mobile view

--- web dev/main.js
+++ web dev/main.js
@@ -1,7 +1,7 @@
 $(window).on("load", function() {
-    setTimeout(function(){ $('.loader-wrapper').fadeOut('slow'); }, 2000);
-    setTimeout(function(){ AOS.init(); },3000);
-    setTimeout(function(){ $('nav').css('visibility', 'visible');},4000);
-    setTimeout(function(){ $('#home').css('visibility', 'visible');},3000);
-    setTimeout(function(){ $('.ct-btn-scroll').css('visibility', 'visible');},3000);
+    setTimeout(function(){ $('.loader-wrapper').fadeOut('slow'); }, 0);
+    setTimeout(function(){ AOS.init(); },0);
+    setTimeout(function(){ $('nav').css('visibility', 'visible');},0);
+    setTimeout(function(){ $('#home').css('visibility', 'visible');},0);
+    setTimeout(function(){ $('.ct-btn-scroll').css('visibility', 'visible');},0);
 });

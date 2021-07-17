ce45c0aa0905e9eb36bf8dacedc8f86e7befe6d1
Srijith Bharadwaj D <srijithdamerla2001@gmail.com>
2021-07-17 17:18:03 +0530

    ictas: Replace rocket by a prettier icon
    Signed-off-by: Srijith Bharadwaj D <srijithdamerla2001@gmail.com>

--- web dev/main.js
+++ web dev/main.js
@@ -4,4 +4,17 @@ $(window).on("load", function() {
     setTimeout(function(){ $('nav').css('visibility', 'visible');},0);
     setTimeout(function(){ $('#home').css('visibility', 'visible');},0);
     setTimeout(function(){ $('.ct-btn-scroll').css('visibility', 'visible');},0);
+    $('#top').css("visibility","hidden");
+});
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 300) {
+        $('#top').css("visibility", "visible");
+    } else {
+        $('#top').css("visibility", "hidden");
+    }
+});
+$('#top').click(function() {
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
 });
